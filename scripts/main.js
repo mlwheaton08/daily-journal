b950c805fabe4042108890e1f0caa0b001b7e7af
mlwheaton08 <mlwheaton06@gmail.com>
2022-08-19 19:57:04 -0500

    daily journal part 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,26 @@
-const entries = [
-    {
-        id: 1,
-        date: "07/26/2025",
-        concept: "Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-    {
-        id: 2,
-        date: "07/26/2025",
-        concept: "Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-    {
-        id: 3,
-        date: "07/26/2025",
-        concept: "Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-    {
-        id: 4,
-        date: "07/26/2025",
-        concept: "Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-    {
-        id: 5,
-        date: "07/26/2025",
-        concept: "Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-    {
-        id: 6,
-        date: "07/26/2025",
-        concept: "Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-    {
-        id: 7,
-        date: "07/26/2025",
-        concept: "Flexbox",
-        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
-        mood: "Sad"
-    },
-]
+import {getJournalEntries} from './entries.js';
+const entries = getJournalEntries();
 
-for (const entry of entries) {
-    if (entry.id%2 !== 0) {
-        console.log(entry);
+let entriesHTML = '';
+
+const returnEntries = (array) => {
+    for (const entry of array) {
+        entriesHTML += `
+        <p class="entry">
+        <span class="entryNumber">Entry #${entry.id}</span>
+        <span class="date">Date: ${entry.date}</span>
+        <span class="mood">Mood: ${entry.mood}</span>
+        <span class="concespant">Concepts: ${entry.concept}</span>
+        <span class="entryText">Entry: ${entry.entry}</span>
+        </p>`
     }
 }
+returnEntries(entries);
+
+document.getElementById('entries').innerHTML = entriesHTML;
+
+
+// maybe do buttons at top right and left of notebook for "entries 1-5" and
+// "entries 6-10" to kind of "turn" pages to nevigate entries, as opposed to
+// the scrolling thing that is proving to be quite difficult.
+// also remember reverse order would be best, so most recent entry is at top
